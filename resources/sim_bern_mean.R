7812cac8d5265651ba28b61794ad3f1abf55679f
Jeff Goldsmith <jeff.goldsmith@columbia.edu>
2022-09-15 09:57:23 -0400

    fix link to git_and_github.pdf

--- resources/sim_bern_mean.R
+++
@@ -1,9 +0,0 @@
-sim_bern_mean = function(n, prob) {
-  
-  sim_data = tibble(
-    x = rbinom(n, 1, prob)
-  )
-  
-  sim_data %>% 
-    summarize(p_hat = mean(x))
-}
--- resources/simulate_nruns.R
+++
@@ -1,6 +0,0 @@
-simulate_nruns = function(n_runs = 100, n, beta0 = 2, beta1 = 3) {
-  
-  rerun(n_runs, sim_regression(n, beta0, beta1)) %>% 
-    bind_rows()
-  
-}
